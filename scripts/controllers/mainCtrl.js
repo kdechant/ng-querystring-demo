493a61e6bb12381590f10e783d899b1737080ccb
Keith Dechant <keith.dechant@gmail.com>
2015-08-04 14:32:43 -0700

    Read filters from the query string

--- scripts/controllers/mainCtrl.js
+++ scripts/controllers/mainCtrl.js
@@ -1,15 +1,30 @@
 'use strict';
 angular.module('ngdemoApp')
-    .controller('MainCtrl', function ($scope, BookService) {
+    .controller('MainCtrl', function ($scope, $location, BookService) {
 
         // a simple filter object, with default filter values
         $scope.filters = { author: '', title: '' };
 
-        
-        $scope.update = function() {
-            BookService.all($scope.filters).then(function(data) {
-                $scope.books = data;
-            });
+        // read filters from the query string, and use them to
+        // replace the default filters
+        var qs = $location.search();
+        for (var fld in $scope.filters) {
+            if (fld in qs) {
+                $scope.filters[fld] = qs[fld];
+            }
         }
-        $scope.update();
+        
+        $scope.update = function(fld) {
+            // update the query string with the new filters
+            if ($scope.filters[fld] != '') {
+                $location.search(fld, $scope.filters[fld]);
+            } else {
+                // remove empty filters
+                $location.search(fld, null);
+            }
+        }
+        
+        BookService.all($scope.filters).then(function(data) {
+            $scope.books = data;
+        });
     });
